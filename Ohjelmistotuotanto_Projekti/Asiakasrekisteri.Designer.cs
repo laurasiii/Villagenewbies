6a2a237e000900da70464412cad1ab8a88e8f804
Tomi Väisänen <Tomi.Vaisanen@edu.savonia.fi>
2020-04-16 10:23:52 +0300

    ok näppäimen muutos

--- Ohjelmistotuotanto_Projekti/Asiakasrekisteri.Designer.cs
+++ Ohjelmistotuotanto_Projekti/Asiakasrekisteri.Designer.cs
@@ -155,7 +155,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(435, 225);
+            this.button1.Location = new System.Drawing.Point(12, 225);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(104, 44);
             this.button1.TabIndex = 19;
